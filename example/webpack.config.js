e96c57b4e72c387c07c5352c21f5b3b1fedb3a8c
Mathieu Maes <mathieu@zimmo.be>
2019-02-06 22:07:43 +0100

    fix travis build

--- example/webpack.config.js
+++ example/webpack.config.js
@@ -1,9 +1,10 @@
+const path = require('path');
 const { HtmlStatsPlugin } = require('../');
 
 module.exports = {
     entry: {
-        page1: './src/page1',
-        page2: './src/page2',
+        page1: path.join(__dirname, './src/page1'),
+        page2: path.join(__dirname, './src/page2'),
     },
     plugins: [
         new HtmlStatsPlugin(),
